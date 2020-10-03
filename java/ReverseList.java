72af06af6d86c5b4b52f481e220369f88fdee6fb
Khushboo <112khushimeh@gmail.com>
2020-10-03 12:16:40 +0530

    added to repo

---
+++ java/ReverseList.java
@@ -0,0 +1,82 @@
+class ReverseList {  
+  
+    class Node{  
+        int data;  
+        Node previous;  
+        Node next;  
+  
+        public Node(int data) {  
+            this.data = data;  
+        }  
+    }  
+  
+
+    Node head, tail = null;  
+  
+  
+    public void addNode(int data) {  
+        Node newNode = new Node(data);  
+        if(head == null) {  
+              
+            head = tail = newNode;  
+             
+            head.previous = null;  
+             
+            tail.next = null;  
+        }  
+        else {  
+      
+            tail.next = newNode;  
+            
+            newNode.previous = tail;  
+           
+            tail = newNode;  
+           
+            tail.next = null;  
+        }  
+    }  
+  
+    //reverse() will reverse the doubly linked list  
+    public void reverse() {    
+        Node current = head, temp = null;  
+        while(current != null) {  
+            temp = current.next;  
+            current.next = current.previous;  
+            current.previous = temp;  
+            current = current.previous;  
+        }  
+ 
+        temp = head;  
+        head = tail;  
+        tail = temp;  
+    }  
+  
+   
+    public void display() {  
+        Node current = head;  
+        if(head == null) {  
+            System.out.println("List is empty");  
+            return;  
+        }  
+  
+        while(current != null) {  
+            System.out.print(current.data + " ");  
+            current = current.next;  
+        }  
+    }  
+  
+    public static void main(String[] args) {  
+  
+        ReverseList dList = new ReverseList();  
+  
+        dList.addNode(1);  
+        dList.addNode(2);  
+        dList.addNode(3);  
+        dList.addNode(4);  
+        dList.addNode(5);   
+        dList.reverse();  
+        System.out.println("\nReversed List: ");  
+        dList.display();  
+    }  
+}  
+
